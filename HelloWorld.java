bd78684bcba0eeae68ee4fa33021359ec1e567a7
Kishore-SCM <kishoredevops681993@gmail.com>
2024-07-13 03:50:23 +0000

    add first hello java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("this is madhu");
+    System.out.println("Hello, Banglore");
+    System.out.println("Hello, canada");
+    System.out.println("Hello, kadapa ");
+    System.out.println("Hello, wed......................");
+  }
+}
